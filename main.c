7d63c2bc5d9b57fb3e1eb4fcd235066da2442076
Not the Juniors <47869208+gmhshornets@users.noreply.github.com>
2019-02-27 16:09:14 -0500

    Period 2 Initial Upload
    Includes the code for testing Lego bot claw for period 2 video

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <kipr/botball.h>
+
+void bot_claw_open();
+void bot_claw_closed();
+int rmotor=0, lmotor=2;//ports
+
+int main()
+{
+    bot_claw_open();
+    msleep(500);
+    while(digital(0) == 0)
+    {
+        mav(rmotor, 920);
+    	mav(lmotor, 800);
+    	msleep(10);
+    }
+    bot_claw_closed();
+    mav(rmotor, -920);
+    mav(lmotor, -800);
+    msleep(3000);
+}
+    
+void bot_claw_open()
+{
+    enable_servos();
+    set_servo_position(1,1600);
+    msleep(100);
+    disable_servos();
+}
+
+void bot_claw_closed()
+{
+    enable_servos();
+    set_servo_position(1,600);
+    msleep(100);
+    disable_servos();
+}
